a788b92fe9114e7cb8c30749c197dca467634c6a
JohnWes7 <1246215651@qq.com>
2021-03-26 09:29:33 +0800

    pipe

---
+++ ScriptKill/Assets/Scripts/PipeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeController : MonoBehaviour
+{
+    [Tooltip("管道预制体")]
+    public GameObject prefab;
+
+    [Tooltip("管道")]
+    public SinglePipe[,] singlePipes;
+
+    [SerializeField,Tooltip("谜题")]
+    public int[,] map = {
+        { 1, 4, 1 },
+        { 3, 1, 2 },
+        { 1, 2, 1 }
+    };
+
+    private void Start()
+    {
+        singlePipes = new SinglePipe[3, 3];
+
+        for (int x = 0; x < map.GetLength(1); x++)
+        {
+            for (int y = 0; y < map.GetLength(0); y++)
+            {
+                GameObject temp = Instantiate<GameObject>(prefab, new Vector3(x, 0, -y), Quaternion.identity);
+                temp.name = map[y, x].ToString();
+            }
+        }
+    }
+}
---
+++ ScriptKill/Assets/Scripts/SinglePipe.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinglePipe : MonoBehaviour
+{
+    [Tooltip("有没有被查找到")]
+    public bool isFind = false;
+    public Vector2 pos;
+    public Vector4 dir;
+
+    
+}
